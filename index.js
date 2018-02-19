79f3121a193b45dd7ccc0195dc8b9789ca53cebc
luisf11 <luisfernandop1110@gmail.com>
2018-02-19 16:42:41 -0400

    [WIP] analyse endpoint

--- index.js
+++ index.js
@@ -7,10 +7,17 @@ const path = require('path');
 const app = express();
 app.use(bodyParser.json());
 
+app.use(express.static('public'));
+
 app.get("/",(req, res)=>{
     res.sendFile(path.join(__dirname+ "/public/index.html"));
     
     });
+
+app.post("/analyse",(req, res)=>{
+    const text = req.body.phrase;
+
+});
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
---
+++ public/script/index.js
@@ -0,0 +1,5 @@
+
+function test(){
+
+    alert("hey listen")
+}
